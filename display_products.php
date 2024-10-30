d12fe16618270af6345e5d261166341a2b89a29d
MaksimTse <maksimplamja@gmail.com>
2024-10-30 09:59:47 +0200

    something went wrong and i fixed it

--- display_products.php
+++ display_products.php
@@ -1,41 +1,36 @@
 <?php
-// Функция для загрузки XML
-function loadXML($file) {
-    if (!file_exists($file)) {
-        die('XML файл не найден.');
-    }
-    return simplexml_load_file($file);
-}
-
-// Функция для преобразования XML в HTML с помощью xsltproc
-function transformXML($xmlFile, $xslFile) {
-    // Команда для выполнения xsltproc
-    $command = "xsltproc $xslFile $xmlFile";
-    $output = [];
-    $returnVar = 0;
-
-    // Выполнение команды
-    exec($command, $output, $returnVar);
-
-    // Проверка на ошибки выполнения
-    if ($returnVar !== 0) {
-        die('Ошибка при преобразовании XML в HTML.');
+function displayXMLAsHTML($xmlFilePath) {
+    if (!file_exists($xmlFilePath)) {
+        echo "Файл XML не найден.";
+        return;
     }
 
-    return implode("\n", $output);
+    $xmlContent = simplexml_load_file($xmlFilePath);
+
+    if ($xmlContent === false) {
+        echo "Ошибка загрузки XML.";
+        return;
+    }
+
+    echo "<div class='xml-content'>";
+
+    function displayNode($node) {
+        echo "<ul>";
+        foreach ($node as $key => $value) {
+            echo "<li><strong>$key:</strong> ";
+            if ($value->count()) {
+                displayNode($value);
+            } else {
+                echo htmlspecialchars($value);
+            }
+            echo "</li>";
+        }
+        echo "</ul>";
+    }
+    displayNode($xmlContent);
+
+    echo "</div>";
 }
 
-// Функция для вывода результата
-function outputResult($html) {
-    header('Content-Type: text/html; charset=UTF-8');
-    echo $html;
-}
-
-// Основной код
-$xmlFile = 'Data.xml';
-$xslFile = 'Data.xsl';
-
-$xml = loadXML($xmlFile);
-$html = transformXML($xmlFile, $xslFile);
-outputResult($html);
+displayXMLAsHTML('SmartStock/data.xml');
 ?>
--- info.php
+++ info.php
@@ -1,3 +1,13 @@
 <?php
-phpinfo();
+$jsonData = file_get_contents('Data.json');
+
+$dataArray = json_decode($jsonData, true);
+
+if (json_last_error() !== JSON_ERROR_NONE) {
+    echo "Ошибка декодирования JSON: " . json_last_error_msg();
+    exit;
+}
+echo "<pre>";
+print_r($dataArray);
+echo "</pre>";
 ?>
